5203170e0f97a40c4445981b9e597d214959f499
Sami Ziani <nudqilnb@sfirstmail.com>
2024-10-14 20:32:04 +0200

    start form

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZBase.Forms;
 
 namespace ZBase
 {
@@ -16,7 +17,7 @@ namespace ZBase
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new Login());
         }
     }
 }
